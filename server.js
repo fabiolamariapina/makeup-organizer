e2244808530bd5874ae68986c7bf8f4d4bdc67ac
fabiolamariapina <fabiolamariapina@gmail.com>
2020-10-29 16:51:29 -0400

    Update server.js
    created basic layout of Index route

--- server.js
+++ server.js
@@ -54,6 +54,10 @@ app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
+// Index
+app.get("/yourCollection", (req, res) => {
+  res.send("Hello World");
+});
 
 //___________________
 //Listener
