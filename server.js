6585a3567c68c1138f7ab3a3cde67c8037be2bc9
fabiolamariapina <fabiolamariapina@gmail.com>
2020-10-29 16:54:36 -0400

    Update server.js
    created basic layout of new route

--- server.js
+++ server.js
@@ -54,9 +54,15 @@ app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
+
 // Index
 app.get("/yourCollection", (req, res) => {
   res.send("Hello World");
+});
+
+// New
+app.get("/yourCollection/new", (req, res) => {
+  res.send("new");
 });
 
 //___________________
