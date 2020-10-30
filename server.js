6606effc9dde17a3b39b9dce872b7cbcaaba7449
fabiolamariapina <fabiolamariapina@gmail.com>
2020-10-29 20:14:37 -0400

    Update server.js
    made the put route update the model in MongoDB

--- server.js
+++ server.js
@@ -112,7 +112,15 @@ app.put("yourCollection/:id", (req, res) => {
   } else {
     req.body.buyMore = false;
   }
-  res.send(req.body);
+  Makeup.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedModel) => {
+      console.log(updatedModel);
+      res.send(updatedModel);
+    }
+  );
 });
 
 // Delete
