7d99306c41d474be4530e9ad7fdf51b3ca0a360c
fabiolamariapina <fabiolamariapina@gmail.com>
2020-11-04 19:09:40 -0500

    moved server.js routes to external controller file

--- controllers/makeup.js
+++ controllers/makeup.js
@@ -1,4 +1,81 @@
 const express = require("express");
 const router = express.Router();
 
+//___________________
+// Routes
+//___________________
+//localhost:3000
+// Welcome / Home Page
+router.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+// Index
+router.get("/yourCollection", (req, res) => {
+  Makeup.find({}, (error, allMakeup) => {
+    res.render("index.ejs", {
+      makeup: allMakeup,
+    });
+  });
+});
+
+// New
+router.get("/yourCollection/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+// Create
+router.post("/yourCollection", (req, res) => {
+  if (req.body.buyMore === "on") {
+    req.body.buyMore = true;
+  } else {
+    req.body.buyMore = false;
+  }
+  Makeup.create(req.body, (error, createdMakeup) => {
+    res.redirect("/yourCollection");
+  });
+});
+
+// Show
+router.get("/yourCollection/:id", (req, res) => {
+  Makeup.findById(req.params.id, (err, foundMakeup) => {
+    res.render("show.ejs", {
+      makeup: foundMakeup,
+    });
+  });
+});
+
+// // Edit
+router.get("/yourCollection/:id/edit", (req, res) => {
+  Makeup.findById(req.params.id, (err, foundMakeup) => {
+    res.render("edit.ejs", {
+      makeup: foundMakeup,
+    });
+  });
+});
+
+// // Put
+router.put("/yourCollection/:id", (req, res) => {
+  if (req.body.buyMore === "on") {
+    req.body.buyMore = true;
+  } else {
+    req.body.buyMore = false;
+  }
+  Makeup.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedMakeup) => {
+      res.redirect("/yourCollection");
+    }
+  );
+});
+
+// Delete
+router.delete("/yourCollection/:id", (req, res) => {
+  Makeup.findByIdAndRemove(req.params.id, (err, data) => {
+    res.redirect("/yourCollection");
+  });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -56,82 +56,82 @@ app.use(express.json()); // returns middleware that only parses JSON - may or ma
 //use method override
 app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
 
-//___________________
-// Routes
-//___________________
-//localhost:3000
-// Welcome / Home Page
-app.get("/", (req, res) => {
-  res.render("home.ejs");
-});
+// //___________________
+// // Routes
+// //___________________
+// //localhost:3000
+// // Welcome / Home Page
+// app.get("/", (req, res) => {
+//   res.render("home.ejs");
+// });
 
-// Index
-app.get("/yourCollection", (req, res) => {
-  Makeup.find({}, (error, allMakeup) => {
-    res.render("index.ejs", {
-      makeup: allMakeup,
-    });
-  });
-});
+// // Index
+// app.get("/yourCollection", (req, res) => {
+//   Makeup.find({}, (error, allMakeup) => {
+//     res.render("index.ejs", {
+//       makeup: allMakeup,
+//     });
+//   });
+// });
 
-// New
-app.get("/yourCollection/new", (req, res) => {
-  res.render("new.ejs");
-});
+// // New
+// app.get("/yourCollection/new", (req, res) => {
+//   res.render("new.ejs");
+// });
 
-// Create
-app.post("/yourCollection", (req, res) => {
-  if (req.body.buyMore === "on") {
-    req.body.buyMore = true;
-  } else {
-    req.body.buyMore = false;
-  }
-  Makeup.create(req.body, (error, createdMakeup) => {
-    res.redirect("/yourCollection");
-  });
-});
+// // Create
+// app.post("/yourCollection", (req, res) => {
+//   if (req.body.buyMore === "on") {
+//     req.body.buyMore = true;
+//   } else {
+//     req.body.buyMore = false;
+//   }
+//   Makeup.create(req.body, (error, createdMakeup) => {
+//     res.redirect("/yourCollection");
+//   });
+// });
 
-// Show
-app.get("/yourCollection/:id", (req, res) => {
-  Makeup.findById(req.params.id, (err, foundMakeup) => {
-    res.render("show.ejs", {
-      makeup: foundMakeup,
-    });
-  });
-});
+// // Show
+// app.get("/yourCollection/:id", (req, res) => {
+//   Makeup.findById(req.params.id, (err, foundMakeup) => {
+//     res.render("show.ejs", {
+//       makeup: foundMakeup,
+//     });
+//   });
+// });
 
-// // Edit
-app.get("/yourCollection/:id/edit", (req, res) => {
-  Makeup.findById(req.params.id, (err, foundMakeup) => {
-    res.render("edit.ejs", {
-      makeup: foundMakeup,
-    });
-  });
-});
+// // // Edit
+// app.get("/yourCollection/:id/edit", (req, res) => {
+//   Makeup.findById(req.params.id, (err, foundMakeup) => {
+//     res.render("edit.ejs", {
+//       makeup: foundMakeup,
+//     });
+//   });
+// });
 
-// // Put
-app.put("/yourCollection/:id", (req, res) => {
-  if (req.body.buyMore === "on") {
-    req.body.buyMore = true;
-  } else {
-    req.body.buyMore = false;
-  }
-  Makeup.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-    (err, updatedMakeup) => {
-      res.redirect("/yourCollection");
-    }
-  );
-});
+// // // Put
+// app.put("/yourCollection/:id", (req, res) => {
+//   if (req.body.buyMore === "on") {
+//     req.body.buyMore = true;
+//   } else {
+//     req.body.buyMore = false;
+//   }
+//   Makeup.findByIdAndUpdate(
+//     req.params.id,
+//     req.body,
+//     { new: true },
+//     (err, updatedMakeup) => {
+//       res.redirect("/yourCollection");
+//     }
+//   );
+// });
 
-// Delete
-app.delete("/yourCollection/:id", (req, res) => {
-  Makeup.findByIdAndRemove(req.params.id, (err, data) => {
-    res.redirect("/yourCollection");
-  });
-});
+// // Delete
+// app.delete("/yourCollection/:id", (req, res) => {
+//   Makeup.findByIdAndRemove(req.params.id, (err, data) => {
+//     res.redirect("/yourCollection");
+//   });
+// });
 
 //___________________
 //Listener
